37bc7207884a8a855ea5582ff62ca485192987bd
longyf <71975617@qq.com>
2023-04-17 02:55:46 +0000

    修正;dataparsing重复deviceshadow工作

--- test/lyf.js
+++ test/lyf.js
@@ -1,3 +1,11 @@
 var getPK = require("../lib/utils/getlist.js").getproductkey
 
 getPK("Ddbf2bbb1a322196b6811c")
+
+
+const test={
+ "state": {
+ "reported": {
+   "payload":"AQEGAAA+L9QBCgEAAQEBBQEGAQcAGgIAAgECBQIGAgcBCgMAAwEDBQMGAwcBCgQABAEEBQQGBAcBCgUABQEFBQUGBQcCAhI0"}
+}
+}
