d5636523a43f50b6177386695a73e2e49b0f3992
longyf <71975617@qq.com>
2023-06-06 12:35:22 +0000

    可以运转的一版，间隔报错

--- test/lyf.js
+++ test/lyf.js
@@ -1,11 +1,15 @@
-var getPK = require("../lib/utils/getlist.js").getproductkey
+// var getPK = require("../lib/utils/getlist.js").getproductkey
 
-getPK("Ddbf2bbb1a322196b6811c")
+// getPK("Ddbf2bbb1a322196b6811c")
 
 
-const test={
- "state": {
- "reported": {
-   "payload":"AQEGAAA+L9QBCgEAAQEBBQEGAQcAGgIAAgECBQIGAgcBCgMAAwEDBQMGAwcBCgQABAEEBQQGBAcBCgUABQEFBQUGBQcCAhI0"}
-}
-}
+// const test={
+//  "state": {
+//  "reported": {
+//    "payload":"AQEGAAA+L9QBCgEAAQEBBQEGAQcAGgIAAgECBQIGAgcBCgMAAwEDBQMGAwcBCgQABAEEBQQGBAcBCgUABQEFBQUGBQcCAhI0"}
+// }
+// }
+
+const js = { product_key: '1b313f4449a637bbbe8ed40c643a691a' } 
+
+console.log(js.product_key)
