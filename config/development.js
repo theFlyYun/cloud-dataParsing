3bdaf6a77c247bfef10e9dd1710bbf5ec2dac253
longyf <71975617@qq.com>
2023-06-06 08:25:29 +0000

    readDevice

--- config/development.js
+++ config/development.js
@@ -16,6 +16,43 @@ module.exports = {
     cluster: false,
     options: [{ 'host': 'localhost', 'port': '6379' }],
   },
+  "databases": [
+    {
+      'db': 'redis',
+      'cluster': false,
+      'options': [{ 'host': 'localhost', 'port': '6379' }],
+    },
+    // {
+    //   "db": "mysql",
+    //   "username": "yh",
+    //   "password": "lbh1234qwer",
+    //   "database": "cloud_http_test",
+    //   "host": "112.74.85.2",
+    //   "dialect": "mysql"
+    // }
+    {
+      'db': 'mysql',
+      'username': 'root',
+      'password': 'root@aliyun',
+      'database': 'cloud_http_server',
+      'host': 'localhost',
+      'dialect': 'mysql',
+      'logging': false,
+      'operatorsAliases': false,
+      'define': {
+        'charset': 'utf8',
+        'dialectOptions': {
+          'collate': 'utf8_general_ci',
+        },
+      },
+      'pool': {
+        'max': 50,
+        'min': 10,
+        'acquire': 20000,
+        'idle': 100000,
+      },
+    },
+  ],
   logger: {
     level: 'debug',
   },
@@ -54,5 +91,11 @@ module.exports = {
   serviceRegistryKey: {
     serviceKey: `${cloudid}:cloudDeviceShadow:-1`,
     providerKey: `${shadowid}:localhost:-1`,
+  },
+  "mongodb": {
+    "host": "localhost",
+    "port": "27017",
+    "db": "mqttLogger",
+    "cluster": false,
   },
 };
--- config/production.js
+++ config/production.js
@@ -12,4 +12,47 @@ module.exports = {
   "logger": {
     "level": "info",
   },
+  "databases": [
+    {
+      'db': 'redis',
+      'cluster': false,
+      'options': [{ 'host': 'localhost', 'port': '6379' }],
+    },
+    // {
+    //   "db": "mysql",
+    //   "username": "yh",
+    //   "password": "lbh1234qwer",
+    //   "database": "cloud_http_test",
+    //   "host": "112.74.85.2",
+    //   "dialect": "mysql"
+    // }
+    {
+      'db': 'mysql',
+      'username': 'root',
+      'password': 'root@aliyun',
+      'database': 'cloud_http_server',
+      'host': 'localhost',
+      'dialect': 'mysql',
+      'logging': false,
+      'operatorsAliases': false,
+      'define': {
+        'charset': 'utf8',
+        'dialectOptions': {
+          'collate': 'utf8_general_ci',
+        },
+      },
+      'pool': {
+        'max': 50,
+        'min': 10,
+        'acquire': 20000,
+        'idle': 100000,
+      },
+    },
+  ],
+  "mongodb": {
+    "host": "localhost",
+    "port": "27017",
+    "db": "mqttLogger",
+    "cluster": false,
+  },
 };
